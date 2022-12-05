2c93b1c2bfd4f467ed045ba0bae0a6b384a6c603
ccp84 <ccp84@icloud.com>
2022-12-05 21:07:14 +0000

    finished lesson 1

--- arrow-functions.js
+++ arrow-functions.js
@@ -5,10 +5,42 @@
 
 // Vanilla JavaScript Function
 
+// function addTwoNumbers (a, b) {
+//     return a + b;
+// }
+
+// let sum = addTwoNumbers(3, 5);
+// console.log(sum);
+
 // Arrow Function With Parameters
+
+const addTwoNumbers = (a, b) => {
+    return a + b;
+}
+
+let sum = addTwoNumbers(3, 5);
+console.log(sum);
 
 // Single Line Arrow Function With Parameters
 
+const addTwoNums = (a, b) => a + b;
+
+let sum2 = addTwoNums(4, 4);
+console.log(sum2);
+
 // Implicit Returns
 
+const sayHi = message => console.log(message);
+
+sayHi('Hello World');
+
 // Returning Multiple Lines
+
+const multiLines = multi => (`
+    return
+    multi
+    lines
+    `);
+
+let multiMessage = multiLines();
+console.log(multiMessage);
